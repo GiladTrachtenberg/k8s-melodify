739b2bd6b3f93765ae388b107764ff622c2f4248
gilad trachtenberg <gilad437@gmail.com>
2022-12-07 16:58:58 +0200

    ..

---
+++ bumpver.sh
@@ -0,0 +1,26 @@
+IFS='.' read -ra ver <<< "$1"
+[[ "${#ver[@]}" -ne 3 ]] && echo "Invalid semver string" && return 1
+[[ "$#" -eq 1 ]] && level='release' || level=$2
+
+release=${ver[2]}
+minor=${ver[1]}
+major=${ver[0]}
+
+case $level in
+  release)
+    release=$((release+1))
+  ;;
+  minor)
+    release=1
+    minor=$((minor+1))
+  ;;
+  major)
+    release=1
+    minor=1
+    major=$((major+1))
+  ;;
+  *)
+    echo "Invalid level passed"
+    return 2
+esac
+echo "$major.$minor.$release"
